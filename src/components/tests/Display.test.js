87e9b37a72f44087abaa375b3bc93ac7dfbc2e81
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-07-19 23:38:53 -0500

    Wrote Tests

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,6 +1,46 @@
+import React from 'react'
+import { screen, render, fireEvent, waitFor } from "@testing-library/react"
+import Display from "../Display"
+import userEvent from "@testing-library/user-event"
 
+const testShow = {
+  //add in approprate test data structure here.
+  name: 'The Test',
+  summary: 'kid takes a test',
+  seasons: [
+      {
+          id: 1,
+          name: 'season1',
+          episodes:[]
+      },
+      {
+          id: 2,
+          name: 'season2',
+          episodes: []
+      }
+  ]
+}
 
+test("Test that the Display component renders without any passed in props", () => {
+  render (<Display />);
+})
 
+test("Test that when the fetch button is pressed, the show component will display", () => {
+  const handleClick = jest.fn()
+  render(<Display show={testShow} handleClick={handleClick}/>);
+
+  const button = screen.queryByRole('button');
+
+  userEvent.click(button)
+})
+
+test("Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.", () => {
+  render(<Display />)
+})
+
+test("Test that when the fetch button is pressed, this function is called.", () => {
+  render(<Display />)
+})
 
 
 
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,43 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    // Arrange
+    render(<Episode episode={testEpisode}/>);
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    // Arrange
+    render(<Episode episode={testEpisode} />)
+
+    const summary = screen.queryByText(/A young boy mysteriously disappears/i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(/the boy's friends conduct their own search/i);
+    expect(summary).not.toHaveTextContent(/billy dies/i);
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+
+    const missingImg = screen.queryByAltText('./stranger_things.png');
+
+    expect(missingImg).toBeInTheDocument();
 })
 
 //Tasks
